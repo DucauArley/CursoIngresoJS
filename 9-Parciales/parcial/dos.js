b8e60207e8f61d54b453f9ab7a1b5617a498e565
DucauArley <36281515+DucauArley@users.noreply.github.com>
2018-02-09 21:19:19 -0300

    ejercicio 1, 2 y 3 del parcial

--- 9-Parciales/parcial/dos.js
+++ 9-Parciales/parcial/dos.js
@@ -1,4 +1,13 @@
 function Mostrar()
 {
-  
+  var importe;
+  var iva;
+  var importeConIva;
+
+  importe = parseInt(prompt("Ingrese el importe de un producto"));
+
+  iva = 0.21;
+  importeConIva = (importe * iva) + importe;
+
+  document.getElementById('importeFinal').value = importeConIva;
 }
--- 9-Parciales/parcial/tres.js
+++ 9-Parciales/parcial/tres.js
@@ -1,4 +1,16 @@
 function Mostrar()
 {
+	var largo;
+	var ancho;
+	var perimetro;
+	var alambre;
+
+	largo = parseInt(document.getElementById('alrgo').value);
+	ancho = parseInt(document.getElementById('ancho').value);
+	perimetro = largo * ancho;
+	alambre = perimetro * 3;
+
+	alert("La cantidad de alambre necesaria es: " + alambre);
+
 
 }
