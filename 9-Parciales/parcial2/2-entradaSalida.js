fd6e41768d3543ab7b61e80aca4ea4c9a749be8b
DucauArley <36281515+DucauArley@users.noreply.github.com>
2018-02-12 00:30:34 -0300

    ejercicios 1, 2 y 3

--- 9-Parciales/parcial2/2-entradaSalida.js
+++ 9-Parciales/parcial2/2-entradaSalida.js
@@ -1,7 +1,14 @@
 //Debemos lograr mostrar un mensaje al presionar el botón  'MOSTRAR'.
 function Mostrar()
 {
-	alert("Funciona 2-EntradaSalida");
-	
-}
+	var importe;
+	var importeFinal;
+	var iva;
 
+	importe =parseInt(prompt("Ingrese el importe", ""));
+	iva = 0.21;
+
+	importeFinal = importe + (importe * iva);
+
+	document.getElementById('importe').value = importeFinal;
+}
--- 9-Parciales/parcial2/3-entradaSalida.js
+++ 9-Parciales/parcial2/3-entradaSalida.js
@@ -1,7 +1,18 @@
 //Debemos lograr mostrar un mensaje al presionar el botón  'MOSTRAR'.
 function Mostrar()
 {
-	alert("Funciona 3-EntradaSalida");
+	var ancho;
+	var largo;
+	var perimetro;
+	var alambre;
+	var alambrado;
 	
-}
+	ancho = parseInt(document.getElementById('ancho').value);
+	largo = parseInt(document.getElementById('largo').value);
 
+	alambre = 6;
+	perimetro = ancho * largo;
+	alambrado = perimetro * alambre;
+
+	alert("La cantidad de alambre necesario para colocar el alambrado es: " + alambrado);
+}
