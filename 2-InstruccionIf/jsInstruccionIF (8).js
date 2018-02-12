506af978a2db4edb9f9296e8af801560827c5e42
DucauArley <36281515+DucauArley@users.noreply.github.com>
2018-02-12 18:39:39 -0300

    ejercicios 7 y 8

--- 2-InstruccionIf/jsInstruccionIF (8).js
+++ 2-InstruccionIf/jsInstruccionIF (8).js
@@ -2,7 +2,18 @@ function Mostrar()
 {
 //tomo la edad  
 
-	
+	var edad;
+	var estadoCivil;
+
+	edad = parseInt(document.getElementById('edad').value);
+	estadoCivil = document.getElementById('estadoCivil').value;
+
+	if (edad > 17 && estadoCivil == "Soltero") 
+	{
+		alert("Es soltero y no es menor");
+	}
+
+
 
 
 }//FIN DE LA FUNCIÓN
