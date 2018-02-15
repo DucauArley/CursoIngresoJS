5c8be33e8b2bcde8ceba288e8f7126290ca34e2c
DucauArley <36281515+DucauArley@users.noreply.github.com>
2018-02-15 19:46:27 -0300

    ejercicios 9 y 10
    Números Random

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -1,6 +1,26 @@
 function Mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
+	var nota;
+
+	document.getElementById("FormIngreso").innerHTML = Math.floor(Math.random() * 10) + 1;
+	nota = document.getElementById('FormIngreso').innerHTML;
+
+	if (nota > 8) 
+	{
+		alert("EXCELENTE" + " " + nota);
+	}
+	else
+	{
+		if (nota > 3  && nota < 9) 
+		{
+			alert("APROBO" + " " + nota);
+		}
+		else
+		{
+			alert("Vamos, la proxima se puede" + " " + nota);
+		}
+	}
 	
 
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (9).js
+++ 2-InstruccionIf/jsInstruccionIF (9).js
@@ -2,5 +2,5 @@ function Mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
 	
-
+	alert(document.getElementById("FormIngreso").innerHTML = Math.floor(Math.random() * 10) + 1);
 }//FIN DE LA FUNCIÓN
