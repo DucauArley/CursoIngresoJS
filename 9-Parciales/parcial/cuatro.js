20fd105d787d154b5b74bab8787cf5501f13effc
DucauArley <36281515+DucauArley@users.noreply.github.com>
2018-02-16 19:44:01 -0300

    ejercicio 4

--- 9-Parciales/parcial/cuatro.js
+++ 9-Parciales/parcial/cuatro.js
@@ -1,4 +1,28 @@
 function Mostrar()
 {
+	var numeroUno;
+	var numeroDos;
+	var resultado;
+
+	numeroUno = parseInt(prompt("Ingrese el numero uno"));
+	numeroDos = parseInt(prompt("Ingrese el numero dos"));
+
+	if (numeroUno == numeroDos) 
+	{
+		resultado = numeroUno * numeroDos;
+		document.write("La multiplicacion es: " + resultado);
+	}
+	else
+	{
+		if (numeroUno > numeroDos) 
+		{
+			resultado = numeroUno - numeroDos;
+			document.write("La resta es: " + resultado);
+		}
+		else
+		{
+			resultado = numeroUno + numeroDos;
+			document.write("La suma es: " + resultado);		}
+	}
 
 }
