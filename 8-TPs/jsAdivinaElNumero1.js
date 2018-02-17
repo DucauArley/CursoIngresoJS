125c7f43993a1402d7622cb088b6618f3409d69c
DucauArley <36281515+DucauArley@users.noreply.github.com>
2018-02-16 21:20:20 -0300

    adivina el numero 1
    Se le agrego que si acierta en el primer intento entonces tiene que volver a jugar para que nadie acierte en el primer intento y se le puso un limite de 10 intentos

--- 8-TPs/jsAdivinaElNumero1.js
+++ 8-TPs/jsAdivinaElNumero1.js
@@ -9,18 +9,47 @@ de no ser igual se debe informar si “falta…”  para llegar al número secre
 */
 var numeroSecreto; 
 var contadorIntentos;
-
+var numeroIngresado;
 function comenzar()
 {
 	//Genero el número RANDOM entre 1 y 100
-	 
-		//alert(numeroSecreto );
-	
-
+	contadorIntentos = 0
+	numeroSecreto = Math.floor(Math.random() * 100) + 1;
+	console.log(numeroSecreto);
 }
 
 function verificar()
 {
-	
-	
+	numeroIngresado = parseInt(document.getElementById('numero').value);
+
+	if (numeroIngresado == numeroSecreto && contadorIntentos < 1) 
+	{
+		alert("Juga de vuelta")
+		contadorIntentos = contadorIntentos + 1;
+		document.getElementById('intentos').value = contadorIntentos;
+	}
+	else
+	{
+		if (contadorIntentos < 10) 
+		{
+			if (numeroIngresado == numeroSecreto) 
+			{
+				alert("Usted es un ganador, y en solo " + contadorIntentos + " intentos");
+			}
+			else
+			{
+				if (numeroIngresado > numeroSecreto) 
+				{
+					alert("Se paso");
+				}
+				else
+				{
+					alert("Falta");
+				}
+			}
+		contadorIntentos = contadorIntentos + 1;
+		document.getElementById('intentos').value = contadorIntentos;
+		}
+ 	}
+ 	
 }
