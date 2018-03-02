75bfeca71cb4a4ba2d7ac94fda551996d772cb39
DucauArley <36281515+DucauArley@users.noreply.github.com>
2018-03-02 13:24:19 -0300

    ejercicio 7

--- 9-Parciales/parcial/siete.js
+++ 9-Parciales/parcial/siete.js
@@ -2,21 +2,54 @@ function Mostrar()
 {
 	var notas;
 	var sexo;
-	var contadorNotas = 0;
+	var contadorNotasVarones = 0;
 	var contador = 0;
+	var sumaNotas = 0;
+	var promedioNotas;
+	var notaMasBaja;
 
-	notas = parseInt(prompt("Ingrese una nota"));
-	sexo = prompt("Ingrese su sexo");
-
-	while(contador < 10 && notas < 10 && notas > 0)
+	while(contador < 100)
 	{
 		contador ++;
 		notas = parseInt(prompt("Ingrese una nota"));
+		sexo = prompt("Ingrese su sexo");
+
+		while(isNaN(notas) || notas > 10 || notas < 0)
+		{
+			notas = parseInt(prompt("Ingrese nuevamente una nota"));
+		}
+
+		while(sexo != "f" && sexo != "m")
+		{
+			sexo = prompt("Ingrese su sexo nuevamente");
+		}
+
+		if (contador == 1) 
+		{
+			notaMasBaja = notas;
+		}
+		else
+		{
+			if (notaMasBaja > notas) 
+			{
+				notaMasBaja = notas;
+			}
+		}
+
+		if (sexo == "m" && notas >= 6) 
+		{
+			contadorNotasVarones ++;
+		}
+
+
+		sumaNotas = notas + sumaNotas;
+
 	}
 
+	promedioNotas = sumaNotas / contador;
 
-
-
+	alert("El promedio de notas es: " + promedioNotas + " La nota mas baja es: " + notaMasBaja + 
+		" La cantidad de varones que su nota haya sido mayor o igual a 6 es: " + contadorNotasVarones);
 
 
 
