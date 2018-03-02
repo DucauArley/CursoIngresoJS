71c48b9c92420e352e90de4a6cc4e98232c9565b
DucauArley <36281515+DucauArley@users.noreply.github.com>
2018-03-02 14:16:22 -0300

    ferrete facturacion y ferrete construccion

--- 8-TPs/jsFerreteConstruccion.js
+++ 8-TPs/jsFerreteConstruccion.js
@@ -6,13 +6,53 @@ C.	Para hacer un contrapiso de 1m x 1m se necesitan 2 bolsas de cemento y 3 de c
 
 function Rectangulo () 
 {
+	var largo;
+	var ancho;
+	var perimetro;
+	var alambre = 3;
+	var alambrado;
+
+	largo = parseInt(document.getElementById('Largo').value);
+	ancho = parseInt(document.getElementById('Ancho').value);
+
+	perimetro = 2 * (largo + ancho);
+
+	alambrado = perimetro * alambre;
+
+	alert("Se necesitan " + alambrado + " metros de alambre")
 
 }
 function Circulo () 
 {
-	
+	var radio;
+	var alambre = 3;
+	var perimetro;
+	var alambrado;
+	var pi = 3.1415;
+
+	radio = parseInt(document.getElementById('Radio').value);
+
+	perimetro = 2 * pi  * radio;
+
+	alambrado = perimetro * alambre;
+
+	alert("Se necesitan " + alambrado + " metros de alambre");
+
 }
 function Materiales () 
 {
-	
+	var largo;
+	var ancho;
+	var cemento;
+	var cal;
+
+	largo = parseInt(document.getElementById('Largo').value);
+	ancho = parseInt(document.getElementById('Ancho').value);
+
+	cemento = largo + ancho;
+	cal = ((largo + ancho) * 3) / 2;
+
+	alert("Se necesitan " + cemento + " bolsas de cemento y " + cal + " bolsas de cal");
+
+
 }
--- 8-TPs/jsFerreteFacturacion.js
+++ 8-TPs/jsFerreteFacturacion.js
@@ -3,15 +3,58 @@ A.	Ingresar tres precios de productos y mostrar la suma de los mismos.
 B.	Ingresar tres precios de productos y mostrar el promedio de los mismos.
 C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
 */
+	
+
 function Sumar () 
 {
-	
+	var precioUno;
+	var precioDos;	
+	var precioTres;
+	var resultado;
+
+	precioUno = parseInt(document.getElementById('PrecioUno').value);
+	precioDos = parseInt(document.getElementById('PrecioDos').value);
+	precioTres = parseInt(document.getElementById('PrecioTres').value);
+
+	resultado = precioUno + precioDos + precioTres;
+
+	alert("La suma es: " + resultado);
 }
 function Promedio () 
 {
+	var precioUno;
+	var precioDos;	
+	var precioTres;
+	var resultado;
+
+	precioUno = parseInt(document.getElementById('PrecioUno').value);
+	precioDos = parseInt(document.getElementById('PrecioDos').value);
+	precioTres = parseInt(document.getElementById('PrecioTres').value);
 	
+	resultado = (precioUno + precioDos + precioTres) / 3;
+
+	alert("El promedio es: " + resultado);
+
 }
 function PrecioFinal () 
 {
-	
+	var precioUno;
+	var precioDos;	
+	var precioTres;
+	var sumaPrecios;
+	var resultado;
+	var IVA = 0.21;
+
+	precioUno = parseInt(document.getElementById('PrecioUno').value);
+	precioDos = parseInt(document.getElementById('PrecioDos').value);
+	precioTres = parseInt(document.getElementById('PrecioTres').value);
+
+	sumaPrecios = (precioUno + precioDos + precioTres);
+
+	resultado = (sumaPrecios * IVA) + sumaPrecios;
+
+
+	alert("El resultado es: " + resultado);
+
+
 }
